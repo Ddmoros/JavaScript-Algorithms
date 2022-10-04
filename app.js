f0ba27ad3b1772966c5a86d88da6458d0fdd87ce
Daniel Moros <113144718+Ddmoros@users.noreply.github.com>
2022-10-03 21:01:18 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,52 @@
+// Create FizzBuzz app //
+
+function fizzBuzz(num){
+    if(num % 3 == 0 && num % 5==0){
+     console.log("fizzbuzz")
+    }
+
+    else if(num % 5 == 0) {
+        console.log("Buzz")
+    }     
+
+    else if(num % 3 == 0){
+        console.log("Fizz")
+    }
+}
+fizzBuzz(10) 
+fizzBuzz(9)
+fizzBuzz(14)
+
+
+// Create removes duplicates from an array //
+
+1et names = ["John","Mary", "Alex", "Steve", "Mary", "John"] 
+1et notduplicates = remove(names)
+
+function remove(array){
+    1et notduplicates = []
+
+// Create a Palindrome app //
+
+function palindrome(string) {
+    1et rev_string = " "
+    for (1et i=string,length-1;i>=0 i--) 
+    {
+        rev-string += string[1]
+    }
+    if (rev_string == string)
+    {
+        console.log("palindrome")
+        
+     else {
+        console.log(" not palindrome")
+    }
+
+}
+
+
+//
+
+
+
+}
